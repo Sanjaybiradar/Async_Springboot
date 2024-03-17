d9d34c2c15810b68c6cdaab29dee3c2b15b0d85c
Sanjaybiradar <bsanjaybiradar7@gmail.com>
2024-03-17 21:47:18 +0530

    Asynchronous Implementation

---
+++ src/main/java/com/learning/asynchronous_springboot/AsynchronousSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.learning.asynchronous_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousSpringbootApplication.class, args);
+	}
+
+}
